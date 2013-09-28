83141993f7aee9fd69c906292055ec612c3be961
jsk <knomedia@gmail.com>
2013-09-27 23:27:45 -0600

    Fix error with published status

--- lib/wp_op/post_builder.rb
+++ lib/wp_op/post_builder.rb
@@ -6,7 +6,7 @@ class PostBuilder
     p = Post.new
     p.title = item.at_css('title').text
     p.content = item.at_css('encoded').text
-    p.published = (item.at_css('status').text == 'draft')
+    p.published = (item.at_css('status').text == 'publish')
     p.date = item.at_css('pubDate').text
     p
   end
